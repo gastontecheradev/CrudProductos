a84945c006c231880e349aaf28494f80f93b7026
gastontecheradev <acorde.musica10@gmail.com>
2026-06-22 19:26:04 -0300

    Commit

---
+++ CrudProductos/Controllers/ProductosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CrudProductos.Data;
+using CrudProductos.Models;
+
+namespace CrudProductos.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
